e6c20f9f0b2225f805c67512be12e77eb21754dd
Rahul Chandankeri <77581043+Rahulchandankeri@users.noreply.github.com>
2021-10-29 14:46:01 +0530

    Necesscary changes

--- main.js
+++ main.js
@@ -1,83 +1,83 @@
 "use strict";
-// let tipSelect = document.querySelectorAll(".tip-select").textContent;
-// let bill, customTip, person;
-// let person = document.getElementById("getPerson").value;
-// getTipValue = Number(document.querySelector("#customTip").value);
-// console.log(person);
-let bill;
-let getTipValue;
-let person;
-let total;
-let total_2;
-let tipValue;
-let split;
+
+//Declare variables
+let bill, getTipValue, person, total, total_2, tipValue, split;
+let peopleContainer = document.querySelector(".total-people");
 let tipPerPerson = (document.querySelector("#total-tip").textContent = 0);
 let totalperPerson = (document.querySelector("#total-amount").textContent = 0);
 
-// Fetch bill input
-let perform = () => {
-  addEventListener("input", function () {
-    bill = Number(document.querySelector("#getBill").value);
-    if (bill === 0) {
-      alert(`Please enter bill`);
-      person = document.getElementById("getPerson").value = null;
-      getTipValue = document.querySelector("#customTip").value = null;
-    }
-    // console.log(bill);
-    return bill;
-  });
-};
-perform();
-
-//Tip selection from button
+//Select tip/taken input from button press - Part-1
 let press = (tipPercent) => {
   getTipValue = document.querySelector("#customTip").value = null;
   person = document.getElementById("getPerson").value = null;
   total = (bill * tipPercent) / 100;
-  // console.log(total);
   return total;
 };
 press();
-// Tip selection from user input
-let custTip = () => {
-  addEventListener("input", function () {
-    getTipValue = Number(document.querySelector("#customTip").value);
-    total_2 = (bill * getTipValue) / 100;
-    // console.log(total);
-    return total_2;
-  });
-};
-custTip();
 
-//Divide Tip for each person
-let people = () => {
+// Intiate
+let perform = () => {
   addEventListener("input", function () {
+    bill = Number(document.querySelector("#getBill").value);
+
+    // Calculate bill only if amount is entered
+    if (bill >= 0) {
+      document.querySelector(".invalid-entry_2").textContent = null;
+      document.querySelector(".Reset-btn").classList.remove("disable-btn");
+    }
+    if (bill === 0) {
+      document.querySelector(".invalid-entry_2").textContent = `Enter bill`;
+      person = document.getElementById("getPerson").value = null;
+      getTipValue = document.querySelector("#customTip").value = null;
+    }
+
+    // Computing tip based on user input Part-2
+    getTipValue = Number(document.querySelector("#customTip").value);
+
+    total_2 = (bill * getTipValue) / 100;
     person = Number(document.getElementById("getPerson").value);
 
+    // Validate
+    if (person >= 0) {
+      document.querySelector(".invalid-entry").textContent = null;
+      peopleContainer.classList.remove("invalid-Person");
+    }
+    if (person <= 0) {
+      peopleContainer.classList.add("invalid-Person");
+      document.querySelector(".invalid-entry").textContent = `Can't be zero`;
+    }
+
+    // Computing tip amount per person
     let final = total / person || total_2 / person;
     tipValue = final.toFixed(2);
+
     if (person >= 1) {
       tipPerPerson = document.querySelector(
         "#total-tip"
       ).textContent = `$${tipValue}`;
-      // console.log(`Tip per person is ${tipValue}`);
+
+      //  Computing total amount payable per person including tip
+      split = Number(bill) / Number(person) + Number(tipValue);
+      if (split >= 1 && person >= 1) {
+        totalperPerson = document.querySelector(
+          "#total-amount"
+        ).textContent = `$${Math.trunc(split)}`;
+      }
+
+      return split;
     }
-    return tipValue;
+  });
+
+  //   Reset
+  document.querySelector(".Reset-btn").addEventListener("click", function () {
+    bill = document.querySelector("#getBill").value = null;
+    person = document.getElementById("getPerson").value = null;
+    tipPerPerson = document.querySelector("#total-tip").textContent = 0;
+    getTipValue = document.querySelector("#customTip").value = null;
+    document.querySelector("#total-amount").textContent = 0;
+    document.querySelector(".invalid-entry").textContent = null;
+    peopleContainer.classList.remove("invalid-Person");
+    document.querySelector(".Reset-btn").classList.add("disable-btn");
   });
 };
-people();
-
-let finalValue = () => {
-  addEventListener("input", function () {
-    split = Number(bill) / Number(person) + Number(tipValue);
-
-    if (split >= 1 && person >= 1) {
-      totalperPerson = document.querySelector(
-        "#total-amount"
-      ).textContent = `$${Math.trunc(split)}`;
-    }
-
-    return split;
-  });
-};
-finalValue();
+perform();
